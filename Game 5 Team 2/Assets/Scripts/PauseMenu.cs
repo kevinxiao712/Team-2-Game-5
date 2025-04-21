c6c8f45bdc5f4a8f9a308059b0148705dd4924e4
Leo Schindler-Gerendasi <dr.wario07@gmail.com>
2025-04-21 17:35:00 -0400

    Paused work on pause menu
    Still mostly want to focus on this more once everything else is in

--- Game 5 Team 2/Assets/Scripts/PauseMenu.cs
+++ Game 5 Team 2/Assets/Scripts/PauseMenu.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public enum GamePhase
+    {
+        Prep,
+        PreShow,
+        Show,
+        Postshow
+    }
+
     [SerializeField]
     private GameObject menu;
+    private GamePhase currentPhase;
+
+    // Kinda scuffed implementation because we don't have a universal manager script
+    [SerializeField]
+    private Canvas preShowCanvas;
+    private GameManager preShowManager;
+    [SerializeField]
+    private Canvas showCanvas;
+    // Insert showManager when that's a thing
+    [SerializeField]
+    private Canvas postShowCanvas; 
+    private PostShowManager postShowManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPhase = GamePhase.Prep;
+        preShowManager = FindAnyObjectByType<GameManager>();
+        postShowManager = FindAnyObjectByType<PostShowManager>();
     }
 
     // Update is called once per frame
@@ -22,5 +44,18 @@ public class PauseMenu : MonoBehaviour
     public void TogglePauseMenu(bool enabled)
     {
         menu.SetActive(enabled);
+        switch (currentPhase)
+        {
+            case GamePhase.Prep:
+                return;
+            case GamePhase.PreShow:
+                preShowCanvas.enabled = false;
+                return;
+        }
+    }
+
+    public void IncrementPhase()
+    {
+        currentPhase = (GamePhase)((int)(currentPhase + 1) % 4);
     }
 }
